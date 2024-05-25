52f3ccd491a174fb0a0af4a6ecb2aaf247c2952b
Indre-V <vilickaite@yahoo.ie>
2024-05-25 20:27:37 +0100

    update: profile models

--- blossom_main/models.py
+++ blossom_main/models.py
@@ -2,7 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
+    """
+    A model representing a user profile.
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
     bio = models.TextField(blank=True)
     profile_picture = models.CharField(max_length=255, blank=True)
     location = models.CharField(max_length=255, blank=True)
@@ -10,4 +15,7 @@ class Profile(models.Model):
     total_favourites = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.user.username
+        """
+        Return the string representation of the associated user's first name if it exists
+        """
+        return f"{self.first_name} {self.last_name}"
