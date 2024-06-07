be698ab77c36fd1d902ee375deacef9629a0cf66
Indre-V <vilickaite@yahoo.ie>
2024-06-07 22:53:18 +0100

    refactor: profile view code

--- blossom_main/models.py
+++ blossom_main/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 # pylint: disable=unused-argument
+# pylint: disable=locally-disabled, no-member
 
 
 class Profile(models.Model):
@@ -13,20 +14,11 @@ class Profile(models.Model):
     A model representing a user profile.
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=150, blank=True)
-    last_name = models.CharField(max_length=150, blank=True)
-    bio = models.TextField(blank=True)
+    bio = models.TextField(max_length=150, blank=True)
     profile_picture = CloudinaryField('image', default='placeholder_profile')
     location = models.CharField(max_length=255, blank=True)
     total_likes = models.IntegerField(default=0)
     total_favourites = models.IntegerField(default=0)
-
-    def __str__(self):
-        """
-        Return the string representation of the associated user's first name if it exists
-        """
-        return f"{self.first_name} {self.last_name}"
-    objects = models.Manager()
 
 
 @receiver(post_save, sender=User)
@@ -36,6 +28,7 @@ def create_user_profile(sender, instance, created, **kwargs):
     """
     if created:
         Profile.objects.create(user=instance)
+
 
 
 @receiver(post_save, sender=User)
