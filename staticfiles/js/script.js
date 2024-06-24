c3b7b80d133ce10863431fe080a543fd3d779b45
Indre-V <vilickaite@yahoo.ie>
2024-06-24 10:36:05 +0100

    remove: dormant files

--- staticfiles/js/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    if (document.querySelector(".modal-body .alert")) {
-        var myModal = new bootstrap.Modal(document.getElementById('messageModal'));
-        myModal.show();
-    }
-});
